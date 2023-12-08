ea7bbc7199de80387c9d35c323a933cf1bbec975
Goofrey <hairline255@gmail.com>
2023-12-08 16:29:25 +0800

    Login-Done

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SchoolProject.Server.Models;
+
+namespace SchoolProject.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SchoolProject.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
